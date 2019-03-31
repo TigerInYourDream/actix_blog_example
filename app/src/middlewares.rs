e33b5d98942a7acd94c57dcb85072cc38fd49bde
TigerInYourDreams <TigerInYourDream@hi.zy>
2019-03-31 08:38:00 +0800

    add routes routes add imple actor for dbexcutor
    Signed-off-by: TigerInYourDreams <TigerInYourDream@hi.zy>

---
+++ app/src/middlewares.rs
@@ -0,0 +1,36 @@
+use crate::jwt;
+use actix_web::middleware::identity::*;
+use actix_web::middleware::{Middleware, Started};
+use actix_web::{HttpRequest, Result};
+
+use crate::controllers::*;
+use db::models::*;
+
+pub struct JWTMiddleWare; // <- my middleware
+
+/// Middleware implementation, middlewares are generic over application state,
+/// so you can access state with `HttpRequest::state()` method.
+impl<S> Middleware<S> for JWTMiddleWare {
+    fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
+        req.identity().and_then(|to| {
+            let token = jwt::decode::<JWTClaims>(&to, "666".as_ref(), &jwt::Validation::default());
+            println!("{:?}", token);
+            if let Ok(t) = token {
+                let refresh = t.claims.refresh;
+                let now =
+                    NaiveDateTime::from_timestamp(Local::now().timestamp(), 6).timestamp() as usize;
+                if now > refresh {
+                    let jwt = JWT {
+                        user_id: t.claims.user_id.to_owned(),
+                    };
+                    req.remember(jwt.token().unwrap());
+                };
+                Some(())
+            } else {
+                req.forget();
+                None
+            }
+        });
+        Ok(Started::Done)
+    }
+}
---
+++ app/src/routes.rs
@@ -0,0 +1,67 @@
+use actix_web::actix::{Actor, Addr, SyncArbiter, SyncContext};
+use actix_web::middleware::identity::*;
+use actix_web::{fs, http::Method, middleware, App};
+use time::Duration;
+
+use crate::api::*;
+use crate::middlewares::*;
+use db::{DbConnecting, DbPool};
+
+pub struct DbExecutor(pub DbPool);
+
+impl Actor for DbExecutor {
+    type Context = SyncContext<Self>;
+}
+
+pub struct AppState {
+    pub pool: Addr<DbExecutor>,
+}
+
+pub fn app() -> App<AppState> {
+    let pool = DbConnecting::establish_pool();
+    let addr = SyncArbiter::start(6, move || DbExecutor(pool.clone()));
+
+    App::with_state(AppState { pool: addr.clone() })
+        .middleware(middleware::Logger::default())
+        .middleware(IdentityService::new(
+            CookieIdentityPolicy::new(&[0; 32])
+                .name("auth")
+                .max_age(Duration::hours(24 * 7))
+                .secure(false),
+        ))
+        .middleware(JWTMiddleWare)
+        .resource("/", |r| r.method(Method::GET).with(home::index))
+        .resource("/search", |r| {
+            r.method(Method::GET).with(search::new);
+        })
+        .resource("/search/{content}", |r| {
+            r.method(Method::GET).with(search::index);
+        })
+        .resource("/articles", |r| {
+            r.method(Method::GET).with(article::index);
+            r.method(Method::POST).with(article::create);
+        })
+        .resource("/articles/new", |r| {
+            r.method(Method::GET).with(article::new);
+        })
+        .resource("/articles/{id}", |r| {
+            r.method(Method::GET).with(article::show);
+        })
+        .resource("/articles/{id}/put", |r| {
+            r.method(Method::POST).with(article::update);
+        })
+        .resource("/articles/{id}/delete", |r| {
+            r.method(Method::POST).with(article::destroy);
+        })
+        .resource("/articles/{id}/edit", |r| {
+            r.method(Method::GET).with(article::edit)
+        })
+        .resource("/users/login", |r| {
+            r.method(Method::GET).with(users::login_show);
+            r.method(Method::POST).with(users::login);
+        })
+        .resource("/users/logout", |r| {
+            r.method(Method::POST).with(users::logout)
+        })
+        .handler("/public", fs::StaticFiles::new("public").unwrap())
+}
